ebf6b0bc39ad01512a2458618a25ecf21b17654f
Erik <33402840+moctezumaerik19@users.noreply.github.com>
2020-10-26 01:08:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ app.use(express.static(__dirname + '/public'));
 
 var db = mysql.createConnection({host: "stock101.mysql.database.azure.com", user: "SoftwareProject@stock101", password: "Stock101", database: "main", port: "3306"});
 
-app.post('/register', (req, res) => {
+//app.post('/register', (req, res) => {
 
-    const username = req.body.username;
-    const password = req.body.password;
+//    const username = req.body.username;
+//    const password = req.body.password;
 
-    db.query(
-        "INSERT INTO main.student (username, password) VALUES (?, ?)",
-        [username, password], 
-        (err, result) =>{
-            console.log(err);
-        }
-    );
-});
+//    db.query(
+//        "INSERT INTO main.student (username, password) VALUES (?, ?)",
+//        [username, password], 
+//        (err, result) =>{
+//            console.log(err);
+//        }
+//    );
+//});
 //uncommented the next 3 lines of code, lets see if it works
 var server = app.listen(app.get('port'), function (){
     console.log('Express server successfully started and is listening on port:' + server.address().port);
@@ -46,6 +46,6 @@ var server = app.listen(app.get('port'), function (){
 //    res.sendFile(path.join(__dirname, home.html));
 //});
 
-app.listen(3001, () => {
-    console.log('we are goin');
-});
+//app.listen(3001, () => {
+//    console.log('we are goin');
+//});

98f88c41ba10d2524ff5fd63a01f7f54527d4727
Thomas Welborn <thomaswelborn@outlook.com>
2020-10-25 23:07:53 -0500

    fixes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/register', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'clien/build', index.html));
+    res.sendFile(path.join(__dirname, '/client/build', index.html));
 });
 
 app.listen(3001, () => {

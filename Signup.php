430ab7ce4c2687da79e01dc085d325f039133174
Joshua Renfro <jrdarknight@sbcglobal.net>
2020-11-14 14:51:04 -0600

    Testing if the signup.php file will call the signup function

--- Signup.php
+++ Signup.php
@@ -2,14 +2,21 @@
 <html>
 <body>
 
+<script src="/Stock101Project/app/signup.js"></script>
+
 <?php
-$name = "";
+$name = $lastname = $email = $pass = $confirmpass = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	$name = $_POST["signupName"];
-	echo $name;
+	$lastname = $_POST["signupLastName"];
+	$email = $_POST["signupEmail"];
+	$pass = $_POST["signupPassword"];
+	$confirmpass = $_POST["signupConfirmpass"];
 }
 ?>
+
+signup(<?php echo $name;?>, <?php echo $lastname;?>, <?php echo email;?>, <?php echo pass;?>, <?php echo confirmpass;?>);
 
 </body>
 </html>

97422f29eb960937d75d47bd8f4224abe236fd32
Joshua Renfro <jrdarknight@sbcglobal.net>
2020-11-15 11:41:29 -0600

    Validation check test

--- app/routes.js
+++ app/routes.js
@@ -81,6 +81,8 @@ router.post('/plswork',function(req,res){
     console.log("CONNECTED");
     var userin = req.body.signupName;
     var passin = req.body.signupPassword;
+    var validpass = req.body.signupConfirmpass;
+    var classin = req.body.signupClassName;
     var sql2 = "INSERT INTO teacher (username, password ) VALUES (?,?)";
 
     console.log("something is happening at least ????");
@@ -88,6 +90,21 @@ router.post('/plswork',function(req,res){
     console.log(passin);
 
     //console.log("TEST");
+
+    if (passin.length === validpass.length) {
+        for (index = 0; index < passin.length; index++) {
+            if (passin[index] !== ' ' || validpass[index] !== ' ') {
+                if (passin[index] !== validpass[index]) {
+                    document.getElementsByName("signupPassword").setCustomValidity("Passwords do not match!");
+                    return;
+                }
+            }
+            else {
+                document.getElementsByName("signupPassword").setCustomValidity("Passwords cannot contain spaces!");
+                return;
+            }
+        }
+    }
     db.query(sql2,[userin,passin], function (err, result) {
         if(err) {
             console.log(err); 
